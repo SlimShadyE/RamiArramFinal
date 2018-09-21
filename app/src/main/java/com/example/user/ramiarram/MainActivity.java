1d3cec5897fcd2fd41aa6616b700c59a76680bb4
user <ramiar2410@gmail.com>
2018-09-21 09:49:21 +0300

    Option menu

--- app/src/main/java/com/example/user/ramiarram/MainActivity.java
+++ app/src/main/java/com/example/user/ramiarram/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.user.ramiarram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import static com.example.user.ramiarram.R.*;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case id.item1:
+                Toast.makeText(this, " Item menu 1", Toast.LENGTH_SHORT).show();
+                break;
+            case id.item2:
+                Toast.makeText(this, " Item menu 2", Toast.LENGTH_SHORT).show();
+                break;
+            case id.item3:
+                Toast.makeText(this, " Item menu 3", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            return true;
+        }
+    }
