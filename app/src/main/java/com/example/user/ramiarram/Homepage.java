ce0afdd274a9452202f662d95d6ebb47a4f03b9a
SlimShadyE <ramiar2410@gmail.com>
2019-02-04 13:31:40 +0200

    profile1

--- app/src/main/java/com/example/user/ramiarram/Homepage.java
+++ app/src/main/java/com/example/user/ramiarram/Homepage.java
@@ -7,15 +7,19 @@ import android.view.View;
 import android.widget.Button;
 
 public class Homepage extends AppCompatActivity implements View.OnClickListener {
-    Button driver, psngr;
+    Button driver, psngr, profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
          driver = (Button) findViewById(R.id.driver);
-         psngr = (Button) findViewById(R.id.psngr);
+         psngr = (Button) findViewById(R.id.psngr2);
+         profile = (Button) findViewById(R.id.profile);
+
         driver.setOnClickListener(this);
         psngr.setOnClickListener(this);
+        profile.setOnClickListener(this);
+
 
     }
 
@@ -27,6 +31,11 @@ public class Homepage extends AppCompatActivity implements View.OnClickListener 
         }
 
         if(v==psngr){
+            Intent i = new Intent (this, FromTo.class);
+            startActivity(i);
+        }
+
+        if(v==profile){
             Intent i = new Intent (this, FromTo.class);
             startActivity(i);
         }
